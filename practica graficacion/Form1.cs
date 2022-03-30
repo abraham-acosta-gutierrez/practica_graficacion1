498589bfee90e67d89f9659188c3bcf80e77b7db
abraham-acosta-gutierrez <55121388+abraham-acosta-gutierrez@users.noreply.github.com>
2022-03-29 18:39:40 -0600

    Agregar archivos de proyecto.

---
+++ practica graficacion/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace practica_graficacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl1.OpenGL;
+            gl.ClearColor(1, 0, 1, 0);
+        }
+
+        private void openGLControl1_Resize(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl1.OpenGL;
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+            gl.Perspective(30.0f, (double)Width / (double)Height, 5, 100.0);
+            gl.LookAt(-5, 5, -5, 0, 0, 0, 0, 1, 0);
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
+
+        private void openGLControl1_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            SharpGL.OpenGL gl = this.openGLControl1.OpenGL;
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+            gl.Translate(-1.5f, 0f, -6f);
+
+            gl.Begin(OpenGL.GL_QUADS);
+            {
+                gl.Vertex(0.0f, 1.0f, 0.0f);
+                gl.Vertex(-1.0f, -1.0f, 0.0f);
+                gl.Vertex(1.0f, -1.0f, 0.0f);
+
+            }
+            gl.End();
+
+
+            gl.Translate(-1.7f, 0f, 0f);
+            gl.Begin(OpenGL.GL_QUADS);
+            {
+                gl.Vertex(-1.0f, 2.0f, 0.0f);
+                gl.Vertex(1.0f, 2.0f, 0.0f);
+                gl.Vertex(1.0f, -2.0f, 0.0f);
+                gl.Vertex(-1.0f, -2.0f, 0.0f);
+            }
+            gl.End();
+
+
+            gl.Translate(2.1f, 0f, 0f);
+            gl.Begin(OpenGL.GL_QUADS);
+            {
+                gl.Vertex(-1.0f, 2.0f, 0.0f);
+                gl.Vertex(1.0f, 2.0f, 0.0f);
+                gl.Vertex(1.0f, -2.0f, 0.0f);
+                gl.Vertex(-1.0f, -2.0f, 0.0f);
+            }
+            gl.End();
+
+
+            gl.Translate(2.1f, 0f, 0f);
+            gl.Begin(OpenGL.GL_QUADS);
+            {
+                gl.Vertex(-1.0f, 2.0f, 0.0f);
+                gl.Vertex(1.0f, 2.0f, 0.0f);
+                gl.Vertex(1.0f, -2.0f, 0.0f);
+                gl.Vertex(-1.0f, -2.0f, 0.0f);
+            }
+            gl.End();
+
+
+            gl.Translate(2.1f, 0f, 0f);
+            gl.Begin(OpenGL.GL_QUADS);
+            {
+                gl.Vertex(-1.0f, 2.0f, 0.0f);
+                gl.Vertex(1.0f, 2.0f, 0.0f);
+                gl.Vertex(1.0f, -2.0f, 0.0f);
+                gl.Vertex(-1.0f, -2.0f, 0.0f);
+            }
+            gl.End();
+            gl.Flush();
+        }
+    }
+}
